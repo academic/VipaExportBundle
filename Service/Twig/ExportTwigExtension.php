c2824f294ebdda84019506b625e26b10dccdcf67
Behram ÇELEN <behramcelen@gmail.com>
2016-06-21 22:08:55 +0300

    creaet export twig extension service

---
+++ Service/Twig/ExportTwigExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ojs\ExportBundle\Service\Twig;
+
+use Symfony\Component\Routing\RouterInterface;
+
+class ExportTwigExtension extends \Twig_Extension
+{
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    public function __construct(RouterInterface $router)
+    {
+        $this->router = $router;
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            new \Twig_SimpleFunction('doajLanguageTransform', array($this, 'doajLanguageTransform')),
+        );
+    }
+
+    /**
+     * @param $locale
+     * @return string
+     */
+    public function doajLanguageTransform($locale)
+    {
+        $transformArray = [
+            'tr' => 'tur',
+            'en' => 'eng',
+            'fr' => 'fre',
+            'de' => 'ger',
+        ];
+        return $transformArray[$locale];
+    }
+
+    public function getName()
+    {
+        return 'api_twig_extension';
+    }
+}
