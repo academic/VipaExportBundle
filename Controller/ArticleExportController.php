d2179a8b7a7b726405bbef404d17b190a6d98782
Behram ÇELEN <behramcelen@gmail.com>
2016-06-22 01:04:12 +0300

    create article export index action

--- Controller/ArticleExportController.php
+++ Controller/ArticleExportController.php
@@ -2,8 +2,11 @@
 
 namespace Ojs\ExportBundle\Controller;
 
+use APY\DataGridBundle\Grid\Source\Entity;
 use Ojs\CoreBundle\Controller\OjsController as Controller;
+use Ojs\JournalBundle\Entity\Article;
 use Symfony\Component\HttpFoundation\Response;
+use APY\DataGridBundle\Grid\Action\MassAction;
 
 class ArticleExportController extends Controller
 {
@@ -12,6 +15,47 @@ class ArticleExportController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('OjsExportBundle:ArticleExport:index.html.twig');
+        $translator = $this->get('translator');
+        $grid = $this->get('grid');
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $source = new Entity(Article::class, 'export');
+        $grid->setSource($source);
+
+        $exportJsonAction = new MassAction($translator->trans('export.as.json'), [
+            $this, 'jsonAction'
+        ]);
+        $grid->addMassAction($exportJsonAction);
+
+        $exportXmlAction = new MassAction($translator->trans('export.as.xml'), [
+            $this, 'xmlAction'
+        ]);
+        $grid->addMassAction($exportXmlAction);
+
+        $exportCrossrefAction = new MassAction($translator->trans('export.as.crossref'), [
+            $this, 'crossrefAction'
+        ]);
+        $grid->addMassAction($exportCrossrefAction);
+
+        return $grid->getGridResponse('OjsExportBundle:ArticleExport:index.html.twig', [
+            'grid'      => $grid,
+            'journal'   => $journal,
+        ]);
+    }
+
+    /**
+     * @param $primaryKeys
+     */
+    public function jsonAction($primaryKeys)
+    {
+    }
+
+    public function xmlAction()
+    {
+
+    }
+
+    public function crossrefAction()
+    {
+
     }
 }
