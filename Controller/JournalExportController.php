18e5cf5702ca4e07f94fe0f3a59c843fd6c66f47
Behram ÇELEN <behramcelen@gmail.com>
2016-06-21 22:11:09 +0300

    journal export doaj support, create doaj template

--- Controller/JournalExportController.php
+++ Controller/JournalExportController.php
@@ -3,6 +3,7 @@
 namespace Ojs\ExportBundle\Controller;
 
 use Ojs\CoreBundle\Controller\OjsController as Controller;
+use Ojs\JournalBundle\Entity\Article;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
@@ -23,13 +24,13 @@ class JournalExportController extends Controller
     public function jsonAction()
     {
         $journal = $this->get('ojs.journal_service')->getSelectedJournal();
-        $journalExport = $this->get('ojs.data_export');
-        $journalExport->setJournal($journal);
-        $jsonJournalData = $journalExport->toJson();
-        $filePath = $journalExport->storeAsFile($jsonJournalData, 'json');
+        $dataExport = $this->get('ojs.data_export');
+        $dataExport->setJournal($journal);
+        $jsonJournalData = $dataExport->journalToJson();
+        $filePath = $dataExport->storeAsFile($jsonJournalData, 'json');
         $explode = explode('/', $filePath);
         $fileName = end($explode);
-        $journalExport->addToHistory($filePath, 'json');
+        $dataExport->addToHistory($filePath, 'json');
         $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$filePath;
         $response = new BinaryFileResponse($file);
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
@@ -43,13 +44,13 @@ class JournalExportController extends Controller
     public function xmlAction()
     {
         $journal = $this->get('ojs.journal_service')->getSelectedJournal();
-        $journalExport = $this->get('ojs.data_export');
-        $journalExport->setJournal($journal);
-        $xmlJournalData = $journalExport->toXml();
-        $filePath = $journalExport->storeAsFile($xmlJournalData, 'xml');
+        $dataExport = $this->get('ojs.data_export');
+        $dataExport->setJournal($journal);
+        $xmlJournalData = $dataExport->journalToXml();
+        $filePath = $dataExport->storeAsFile($xmlJournalData, 'xml');
         $explode = explode('/', $filePath);
         $fileName = end($explode);
-        $journalExport->addToHistory($filePath, 'xml');
+        $dataExport->addToHistory($filePath, 'xml');
         $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$filePath;
         $response = new BinaryFileResponse($file);
         $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
@@ -57,8 +58,27 @@ class JournalExportController extends Controller
         return $response;
     }
 
+    /**
+     * @return BinaryFileResponse
+     */
     public function doajAction()
     {
+        $em = $this->getDoctrine()->getManager();
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $dataExport = $this->get('ojs.data_export');
+        $dataExport->setJournal($journal);
+        $articles = $em->getRepository(Article::class)->findAll();
+        $doajJournalData = $this->renderView('OjsExportBundle:JournalExport:doaj.xml.twig', [
+            'articles' => $articles,
+        ]);
+        $filePath = $dataExport->storeAsFile($doajJournalData, 'xml');
+        $explode = explode('/', $filePath);
+        $fileName = end($explode);
+        $dataExport->addToHistory($filePath, 'doaj');
+        $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$filePath;
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
 
+        return $response;
     }
 }
