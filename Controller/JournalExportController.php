71eb68d079cc1ed523464f5af7e8b9690c0d6201
Behram ÇELEN <behramcelen@gmail.com>
2016-06-21 18:25:51 +0300

    journal export json and xml support

--- Controller/JournalExportController.php
+++ Controller/JournalExportController.php
@@ -3,7 +3,9 @@
 namespace Ojs\ExportBundle\Controller;
 
 use Ojs\CoreBundle\Controller\OjsController as Controller;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class JournalExportController extends Controller
 {
@@ -13,5 +15,50 @@ class JournalExportController extends Controller
     public function indexAction()
     {
         return $this->render('OjsExportBundle:JournalExport:index.html.twig');
+    }
+
+    /**
+     * @return BinaryFileResponse
+     */
+    public function jsonAction()
+    {
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $journalExport = $this->get('ojs.data_export');
+        $journalExport->setJournal($journal);
+        $jsonJournalData = $journalExport->toJson();
+        $filePath = $journalExport->storeAsFile($jsonJournalData, 'json');
+        $explode = explode('/', $filePath);
+        $fileName = end($explode);
+        $journalExport->addToHistory($filePath, 'json');
+        $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$filePath;
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
+
+        return $response;
+    }
+
+    /**
+     * @return BinaryFileResponse
+     */
+    public function xmlAction()
+    {
+        $journal = $this->get('ojs.journal_service')->getSelectedJournal();
+        $journalExport = $this->get('ojs.data_export');
+        $journalExport->setJournal($journal);
+        $xmlJournalData = $journalExport->toXml();
+        $filePath = $journalExport->storeAsFile($xmlJournalData, 'xml');
+        $explode = explode('/', $filePath);
+        $fileName = end($explode);
+        $journalExport->addToHistory($filePath, 'xml');
+        $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$filePath;
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
+
+        return $response;
+    }
+
+    public function doajAction()
+    {
+
     }
 }
