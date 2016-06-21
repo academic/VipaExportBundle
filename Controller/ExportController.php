582c95e525335a881e2daee69a57365f102e5ebf
Behram ÇELEN <behramcelen@gmail.com>
2016-06-21 18:25:13 +0300

    export file history download action support

--- Controller/ExportController.php
+++ Controller/ExportController.php
@@ -5,7 +5,11 @@ namespace Ojs\ExportBundle\Controller;
 use APY\DataGridBundle\Grid\Column\ActionsColumn;
 use APY\DataGridBundle\Grid\Source\Entity;
 use Ojs\CoreBundle\Controller\OjsController as Controller;
+use Ojs\ExportBundle\Entity\DataExport;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
 class ExportController extends Controller
 {
@@ -25,9 +29,29 @@ class ExportController extends Controller
         );
         $actionColumn->setRowActions($rowAction);
         $grid->addColumn($actionColumn);
+
         return $grid->getGridResponse('OjsExportBundle:Export:history.html.twig', [
             'grid'      => $grid,
             'journal'   => $journal,
         ]);
+    }
+
+    /**
+     * Export Download
+     *
+     * @param Request $request
+     * @param DataExport $dataExport
+     *
+     * @return BinaryFileResponse
+     */
+    public function downloadAction(Request $request, DataExport $dataExport)
+    {
+        $explode = explode('/', $dataExport->getPath());
+        $fileName = end($explode);
+        $file = $this->getParameter('kernel.root_dir'). '/../web/uploads/data_export/'.$dataExport->getPath();
+        $response = new BinaryFileResponse($file);
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $fileName);
+
+        return $response;
     }
 }
